0a20395af70f017ee92a2c654d3b2effec1c71b0
Farid-Efrei <128361230+Farid-Efrei@users.noreply.github.com>
2024-12-08 12:59:30 +0100

    RouterLink et routes en place

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  //Pour que le bouton puisse amener à la bonne route on a besoin d'importer le router depuis le constructeur pour faire la redirection:
+  constructor(private router: Router) {}
+  onContinue(): void {
+    this.router.navigateByUrl('photos');
+  }
 }
